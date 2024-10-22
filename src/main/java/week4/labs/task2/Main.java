a20411d1c95fa9297402162872d037eda40b54dd
edadic <eldar.dadic@stu.ibu.edu.ba>
2024-10-22 16:01:01 +0200

    week 4 tasks

--- src/main/java/week4/labs/task2/Main.java
+++ src/main/java/week4/labs/task2/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         Circle circle = new Circle("red", FillType.FILLED, 5);
-        System.out.println("Circle: ");
+        System.out.println("CIRCLE: ");
         System.out.println(circle);
 
         Rectangle rectangle = new Rectangle("blue", FillType.NOT_FILLED, 5, 10);
-        System.out.println("Rectangle: ");
+        System.out.println("\nRECTANGLE: ");
         System.out.println(rectangle);
 
 
