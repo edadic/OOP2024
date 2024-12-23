1314adbce513e74ebf43f283ba9f5ebff59a5e80
edadic <eldar.dadic@stu.ibu.edu.ba>
2024-12-23 12:42:06 +0100

    Week 12 tasks

---
+++ src/main/java/week12/labs/ChatApplication.java
@@ -0,0 +1,9 @@
+package week12.labs;
+
+public class ChatApplication {
+    public static void main(String[] args) {
+        ChatServer chatServer = new ChatServer();
+        int port = 12345;
+        chatServer.start(port);
+    }
+}
